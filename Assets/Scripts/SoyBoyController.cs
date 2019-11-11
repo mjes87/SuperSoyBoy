522e240ad1750c2d5bf7af76db1cbb03f360d786
Mike Sullivan <mjes87@gmail.com>
2019-11-11 16:02:33 -0500

    Adjusted Air Acceleration

--- Assets/Scripts/SoyBoyController.cs
+++ Assets/Scripts/SoyBoyController.cs
@@ -39,6 +39,7 @@ public class SoyBoyController : MonoBehaviour
     public bool isJumping;
     public float jumpSpeed = 8f;
     public float jumpDurationThreshold = 0.25f;
+    public float airAccel = 3f;
 
     private Vector2 input;
     private SpriteRenderer sr;
@@ -125,9 +126,18 @@ public class SoyBoyController : MonoBehaviour
 
     void FixedUpdate()
     {
-        var acceleration = accel;
+        var acceleration = 0f;
+        if (PlayerIsOnGround())
+        {
+            acceleration = accel;
+        }
+        else
+        {
+            acceleration = airAccel;
+        }
+
         var xVelocity = 0f;
-        if (input.x == 0)
+        if (PlayerIsOnGround() && input.x == 0)
         {
             xVelocity = 0f;
         }
