a729789f70113d504bf66248eeea6322dbc9cbab
Mike Sullivan <mjes87@gmail.com>
2019-11-11 15:47:37 -0500

    Imported Chapter 13 Starter Project

--- Assets/Scripts/SoyBoyController.cs
+++ Assets/Scripts/SoyBoyController.cs
@@ -1,9 +1,37 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿/*
+ * Copyright (c) 2018 Razeware LLC
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish, 
+ * distribute, sublicense, create a derivative work, and/or sell copies of the 
+ * Software in any work that is designed, intended, or marketed for pedagogical or 
+ * instructional purposes related to programming, coding, application development, 
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works, 
+ * or sale is expressly withheld.
+ *    
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
-[RequireComponent(typeof(SpriteRenderer), typeof(Rigidbody2D),
-typeof(Animator))]
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SpriteRenderer), typeof(Rigidbody2D), typeof(Animator))]
 public class SoyBoyController : MonoBehaviour
 {
     public float speed = 14f;
@@ -20,37 +48,30 @@ public class SoyBoyController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
+    // Use this for initialization
+    void Start () {
+	
+	}
 
     // Update is called once per frame
     void Update()
     {
+        input.x = Input.GetAxis("Horizontal");
+        input.y = Input.GetAxis("Jump");
+        if (input.x > 0f)
         {
-            // 1
-            input.x = Input.GetAxis("Horizontal");
-            input.y = Input.GetAxis("Jump");
-            // 2
-            if (input.x > 0f)
-            {
-                sr.flipX = false;
-            }
-            else if (input.x < 0f)
-            {
-                sr.flipX = true;
-            }
+            sr.flipX = false;
+        }
+        else if (input.x < 0f)
+        {
+            sr.flipX = true;
         }
     }
 
     void FixedUpdate()
     {
-        // 1
         var acceleration = accel;
         var xVelocity = 0f;
-        // 2
         if (input.x == 0)
         {
             xVelocity = 0f;
@@ -59,10 +80,8 @@ public class SoyBoyController : MonoBehaviour
         {
             xVelocity = rb.velocity.x;
         }
-        // 3
-        rb.AddForce(new Vector2(((input.x * speed) - rb.velocity.x)
-        * acceleration, 0));
-        // 4
+
+        rb.AddForce(new Vector2(((input.x * speed) - rb.velocity.x) * acceleration, 0));
         rb.velocity = new Vector2(xVelocity, rb.velocity.y);
     }
 }
