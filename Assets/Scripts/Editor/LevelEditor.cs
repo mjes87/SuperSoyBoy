e970683a45621a7325c9caad35d30996b5062efc
Mike Sullivan <mjes87@gmail.com>
2019-11-22 13:44:25 -0500

    Added Level Scripts

---
+++ Assets/Scripts/Editor/LevelEditor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+[CustomEditor(typeof(Level))]
+public class LevelEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Save level"))
+        {
+        }
+    }
+}
